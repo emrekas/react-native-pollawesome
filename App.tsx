eb8c795283af8c79ca7d43eaaafc81cfb5294ad5
Emre <emre@edakik.com>
2020-08-19 17:48:57 +0300

    eslint ve prettier config duzenlendi

--- App.tsx
+++ App.tsx
@@ -1,26 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { SafeAreaView } from "react-native";
+import React, { useState } from 'react';
 
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const fetchFonts = () => Font.loadAsync({
+  'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+  'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
     return (
-      <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }}>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
     );
   }
+
+  if (!isLoadingComplete) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Navigation colorScheme={colorScheme} />
+      <StatusBar />
+    </SafeAreaView>
+  );
 }
--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,6 +1,8 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
 
 import { RootStackParamList } from '../types';
 
