9c987d0747d8cc3bc200ad22b7f5555e755ae2e9
Emre <emre@edakik.com>
2020-08-19 17:49:07 +0300

    login screen yapildi

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,66 +1,139 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button, Keyboard, Platform, KeyboardAvoidingView, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Keyboard,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 
 const LoginScreen = () => {
-    return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS == "ios" ? "padding" : "height"}
-            style={{ flex: 1 }}
-        >
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.container}>
-                    <Text style={styles.head}>Login</Text>
-                    <View style={styles.input}>
-                        <TextInput placeholder="Username" />
-                    </View>
-                    <View style={styles.input}>
-                        <TextInput placeholderTextColor="white" placeholder="Password" secureTextEntry={true} />
-                    </View>
-                    <TouchableOpacity style={styles.button} onPress={() => { }} >
-                        <Text style={styles.buttonText}>Bas Bana</Text>
-                    </TouchableOpacity>
-                </View>
-            </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-    )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const emailValidate = (value: string) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(email).toLowerCase())) {
+    }
+  };
 
-export default LoginScreen
+  const handleSubmit = () => {};
+
+  return (
+    <ImageBackground
+      source={require('../assets/images/loginBackground.jpg')}
+      resizeMode="cover"
+      style={styles.image}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <View style={styles.headContainer}>
+              <Text style={styles.head}>{'Poll\nAwesome'}</Text>
+            </View>
+            <View style={styles.inputContainer}>
+              <View style={styles.input}>
+                <TextInput
+                  keyboardType="email-address"
+                  textContentType="emailAddress"
+                  autoCompleteType="email"
+                  value={email}
+                  placeholderTextColor="gray"
+                  placeholder="Email"
+                  onChangeText={(value: string) => emailValidate(value)}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={password}
+                  placeholderTextColor="gray"
+                  placeholder="Password"
+                  secureTextEntry
+                  onChangeText={(value: string) => setPassword(value)}
+                />
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.button}
+                onPress={handleSubmit}
+              >
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+};
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FF9447'
-    },
-    head: {
-        fontSize:28
-    },
-    input: {
-        justifyContent: 'center',
-        borderWidth: 1,
-        borderColor: 'black',
-        paddingHorizontal: 8,
-        width: 200,
-        height: 42,
-        borderRadius: 6,
-        marginVertical: 8
-    },
-    button: {
-        borderRadius: 8,
-        borderWidth: 1,
-        backgroundColor: '#F96900',
-        padding: 8,
-        paddingHorizontal: 16,
-        width: 140,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        lineHeight: 18,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+  },
+  image: {
+    height: '100%',
+  },
+  headContainer: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  head: {
+    fontSize: 42,
+    color: 'white',
+    fontFamily: 'open-sans-bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flex: 4,
+    alignItems: 'center',
+  },
+  input: {
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'rgba(200, 150, 150, .9)',
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    width: '90%',
+    minWidth: '80%',
+    height: 48,
+    borderRadius: 24,
+    marginVertical: 8,
+  },
+  button: {
+    marginTop: 18,
+    borderRadius: 24,
+    height: 48,
+    borderWidth: 1,
+    borderColor: 'rgba(200, 150, 150, .9)',
+    backgroundColor: '#F96900',
+    padding: 8,
+    paddingHorizontal: 16,
+    width: '80%',
+    minWidth: '70%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    lineHeight: 18,
+    fontWeight: 'bold',
+  },
+});
