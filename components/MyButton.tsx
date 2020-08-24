9563e7dfe33b35a1028d1e75b8a8db1f84d025e1
Emre <emre@edakik.com>
2020-08-24 17:01:51 +0300

    CustomButton için görünümü güzelleştirildi(bence :))

--- components/MyButton.tsx
+++ components/MyButton.tsx
@@ -5,52 +5,51 @@ import {
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const MyButton = (props: any) => (
-  <TouchableOpacity
-    activeOpacity={0.8}
-    style={{ ...styles.button, ...props.buttonStyle }}
-    // onPress={props.onPress}
+  <View
+    style={{
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginVertical: '5%',
+    }}
   >
-    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-      <View style={styles.iconBox}>
-        <MaterialCommunityIcons name={props.iconName} color="white" size={24} />
-      </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'orange',
-          height: 40,
-          right: 15,
-          borderRadius: 20,
-        }}
-      >
-        <Text style={{ ...styles.buttonText, ...props.textStyle }}>
-          {props.text}
-        </Text>
-      </View>
+    <View style={styles.iconBox}>
+      <MaterialCommunityIcons name={props.iconName} color="white" size={24} />
     </View>
-  </TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={{ ...styles.button, ...props.buttonStyle }}
+      onPress={props.onPress}
+    >
+      <Text style={{ ...styles.buttonText, ...props.textStyle }}>
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  </View>
 );
 
 export default MyButton;
 
 const styles = StyleSheet.create({
   button: {
-    marginTop: 18,
     borderRadius: 24,
-    width: '100%',
     minWidth: '70%',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+    height: 40,
+    right: 15,
+    backgroundColor: 'orange',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 12,
+    elevation: 8,
   },
   buttonText: {
     color: '#fff',
-    fontSize: 16,
-    lineHeight: 18,
+    fontSize: 18,
+    lineHeight: 20,
     fontWeight: 'bold',
-    margin: '10%',
   },
   iconBox: {
     width: 60,
@@ -58,8 +57,9 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: '#d07001',
     left: 15,
-    zIndex: 99,
+    zIndex: 999,
+    elevation: 9,
   },
 });
